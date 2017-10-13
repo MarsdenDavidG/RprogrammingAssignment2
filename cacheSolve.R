e405f37e895445800e6e0b70b36f1aa848db6d4c
MarsdenDavidG <32742582+MarsdenDavidG@users.noreply.github.com>
2017-10-13 10:56:08 -0500

    Update cacheSolve.R

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,5 +1,5 @@
 #This computes the inverse of the matrix or retrieves it from cache.
-
+#This requires the makeCacheMatrix function which requires the matlib library.
 cacheSolve <- function(x, ...) {
   I <- x$getInv()
   if(!is.null(I)) {
